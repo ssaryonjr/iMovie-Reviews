1020c2018fa4228d07ccebb18c5f2ed4f99d3e36
Sam Saryon <ssaryonjr@gmail.com>
2022-05-09 06:04:14 -0700

    more updates

--- src/main.js
+++ src/main.js
@@ -5,14 +5,46 @@ searchIcon.addEventListener('click', function() {
 })
 
 
-// window.addEventListener('resize', magic)
 
-// function magic(e){
-//     // console.log(window.innerWidth)
-//     if(window.innerWidth < 1000) {
-//         searchIcon.removeEventListener('mouseover', function() {
-//             searchBox.classList.toggle('active')
-//         })
-//     }
-//     else console.log("I'm so big!")
-// }
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    490: {
+        slidesPerView: 2,
+        spaceBetween: 25
+      },
+
+    700: {
+        slidesPerView: 3,
+        spaceBetween: 15
+      },
+    // when window width is >= 440px
+    1025: {
+      slidesPerView: 4,
+      spaceBetween: 15
+    },
+    // when window width is >= 660px
+    1300: {
+      slidesPerView: 5,
+      spaceBetween: 15
+    },
+    // when window width is >= 100px
+    1600: {
+      slidesPerView: 6,
+      spaceBetween: 15
+    }
+  }
+  });
