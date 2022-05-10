8013f95a3ab8e6e19e042c076708f9ee74e3fe1c
Sam Saryon <ssaryonjr@gmail.com>
2022-05-09 22:56:05 -0700

    Issue: Movie Posters from API are stretching then later fitting into carousel after being resized

--- src/main.js
+++ src/main.js
@@ -4,18 +4,50 @@ searchIcon.addEventListener('click', function() {
     searchBox.classList.toggle('active')
 })
 
+fillData()
 
 
-const swiper = new Swiper('.swiper', {
+const tvSlide = new Swiper('.tv-slide', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
+
+    // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    490: {
+        slidesPerView: 2,
+        spaceBetween: 25
+      },
+
+    700: {
+        slidesPerView: 3,
+        spaceBetween: 15
+      },
+    // when window width is >= 440px
+    1025: {
+      slidesPerView: 4,
+      spaceBetween: 15
+    },
+    // when window width is >= 660px
+    1300: {
+      slidesPerView: 5,
+      spaceBetween: 15
+    },
+    // when window width is >= 100px
+    1600: {
+      slidesPerView: 6,
+      spaceBetween: 15
+    }
+  }
+  });
+
+  const movieSlide = new Swiper('.movie-slide', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
     // Default parameters
   slidesPerView: 1,
@@ -55,7 +87,10 @@ const swiper = new Swiper('.swiper', {
     fetch(`https://www.omdbapi.com/?apikey=b1f6b3a2&s=last&type=movie&plot=full`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data.Search)
+      // console.log(data.Search)
+      data.Search.forEach(movie => {
+        console.log(movie)
+      });
 
     })
     .catch(err => {
@@ -63,6 +98,3 @@ const swiper = new Swiper('.swiper', {
     });
 
 }
-
-
-  fillData()
